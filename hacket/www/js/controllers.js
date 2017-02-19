9b0b7a0e0cdfc707e6a534cabcd37fb04ef33843
Eddie Tapia <Eddie Tapia>
2017-02-18 16:56:24 -0800

    improved UI/UX and have alert messages for turn signals

--- hacket/www/js/controllers.js
+++ hacket/www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('app.controllers', [])
-  
+
 .controller('hacketCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -7,15 +7,64 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('bikeSessionCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
 
+  function ($scope, $stateParams) {
+    $scope.stopCtrl = function(ev) {
+      // Alert the cars that we are slowing down
+      alert("Slowing down");
+    }
 
-}])
-   
+    $scope.rightCtrl = function(ev){
+      // Alert the cars that we are going right
+      alert("Turning right");
+    }
+    $scope.leftCtrl = function(ev) {
+      // Alert the cars that we are going left
+      alert("Turning left");
+    }
+  }
+])
+
+.controller('stopCtrl', function($scope, $ionicActionSheet) {
+
+   $scope.triggerActionSheet = function() {
+
+      // Show the action sheet
+      var showActionSheet = $ionicActionSheet.show({
+         buttons: [
+            { text: 'Edit 1' },
+            { text: 'Edit 2' }
+         ],
+
+         destructiveText: 'Delete',
+         titleText: 'Action Sheet',
+         cancelText: 'Cancel',
+
+         cancel: function() {
+            // add cancel code...
+         },
+
+         buttonClicked: function(index) {
+            if(index === 0) {
+               // add edit 1 code
+            }
+
+            if(index === 1) {
+               // add edit 2 code
+            }
+         },
+
+         destructiveButtonClicked: function() {
+            // add delete code..
+         }
+      });
+   };
+})
+
 .controller('musicSessionCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -23,7 +72,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('hacket2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -31,7 +80,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('signOutCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -39,4 +88,27 @@ function ($scope, $stateParams) {
 
 
 }])
- 
+
+.controller('february172017Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+
+.controller('february152017Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+
+.controller('february142017Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
--- hacket/www/js/routes.js
+++ hacket/www/js/routes.js
@@ -7,10 +7,10 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-  
 
-      .state('hacket2.hacket', {
+
+
+  .state('hacket2.hacket', {
     url: '/homepage',
     views: {
       'side-menu21': {
@@ -29,6 +29,15 @@ angular.module('app.routes', [])
       }
     }
   })
+
+  /*.state('hacket2.bikeSession.stopCtrl', {
+    views: {
+      'side-menu':{
+        templateUrl: 'templates/bikeSession.html',
+        controller: 'bikeSessionCtrl'
+      }
+    }
+  })*/
 
   .state('hacket2.musicSession', {
     url: '/musicsession',
@@ -56,8 +65,38 @@ angular.module('app.routes', [])
     }
   })
 
+  .state('hacket2.february172017', {
+    url: '/feb17',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/february172017.html',
+        controller: 'february172017Ctrl'
+      }
+    }
+  })
+
+  .state('hacket2.february152017', {
+    url: '/feb15',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/february152017.html',
+        controller: 'february152017Ctrl'
+      }
+    }
+  })
+
+  .state('hacket2.february142017', {
+    url: '/feb14',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/february142017.html',
+        controller: 'february142017Ctrl'
+      }
+    }
+  })
+
 $urlRouterProvider.otherwise('/menu/homepage')
 
-  
+
 
 });
